9f4284eae7b8b3bec887d9871078f0437c7509e6
Grobax <43875102+GrobaxGrappleBlast@users.noreply.github.com>
2024-05-22 23:10:59 +0200

    added pre origin logic,

--- src/Modules/ObsidianUI/UIInterfaces/Designer01/SystemDesigner/views/OriginEditor.svelte
+++ src/Modules/ObsidianUI/UIInterfaces/Designer01/SystemDesigner/views/OriginEditor.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import { GrobDerivedNode, TTRPGSystem} from "../../../../../../../src/Modules/Designer";
+    import { onMount } from "svelte";
+    import { GrobDerivedNode, GrobDerivedOrigin, TTRPGSystem, type GrobNodeType} from "../../../../../../../src/Modules/Designer";
    
 	import './OriginEditor.scss'
     import OriginRow from "./OriginRow.svelte";
+    import { slide } from "svelte/transition";
 
 	export let node :GrobDerivedNode;
 	export let system:TTRPGSystem;
@@ -11,12 +13,83 @@
 	let resultSucces : boolean | number = NaN ;
 	let calc 	= node.calc;
 
+	let origins : Record<string , {obj:GrobDerivedOrigin | null, inCalc:boolean}  > = {}
+	let pre_origins : string[] = []
+	onMount(()=>{ 
+		node.origins.forEach( origin  => {
+			origins[origin.symbol] ={ obj: origin , inCalc:node.calc.contains(origin.symbol) };
+		});
+	})
+
 	function testCalc ( e ){
 
+		// test if the calculation is valid
 		let calcValue = e.target.value ;
 		let res = node.testCalculate( calcValue );
 		result = res.value;
 		resultSucces = res.success;
+
+		
+		// Get origins from the calculation
+		let symbolsRes = node.parseCalculationToOrigins( calcValue )
+		if (!symbolsRes)
+			return;
+		
+		// remove from preorigins 
+		let preOriginRemoves :string [] = [];
+		pre_origins.forEach( o  => {
+			if (!calc.contains(o))
+				preOriginRemoves.push(o);
+		});
+		preOriginRemoves.forEach( o => {
+			pre_origins.remove(o);
+		})
+		
+		// if this cant be calculated do not continue
+		if (!resultSucces){
+			pre_origins = pre_origins;
+			return;
+		}
+
+
+		// add symbols
+		symbolsRes.symbolsToAdd.forEach( symbol => {
+			
+			// if it already exists, re-activate it
+			let orig = origins[symbol];
+			if (orig){
+				origins[symbol].inCalc = true;
+				return;
+			}
+
+			// else create a new "temp row"
+			if (!pre_origins.contains(symbol))
+				pre_origins.push(symbol);
+		});
+		
+		// remove symbols
+		symbolsRes.symbolsToAdd.forEach( symbol => {
+			let orig = origins[symbol];
+			if (orig){
+				origins[symbol].inCalc = false;
+			}
+			 
+		}); 
+
+		
+
+		pre_origins = pre_origins;
+	}
+
+	function fromPreOriginToOrigin( preorigin ){
+		 
+		if (!pre_origins.contains(preorigin))
+			return;
+
+		pre_origins.remove(preorigin);
+		origins[preorigin] = {obj:null, inCalc:true};
+		origins = origins;
+		pre_origins=pre_origins;
 	}
 
 </script>
@@ -28,11 +101,22 @@
 		<div class="derivedCalcStatementResult" data-succes={resultSucces} >{result}</div>
 	</div>
 	<div class="derivedOriginRowsContainer">
+		{#each Object.entries(origins) as [symbol, obj]}
+			<OriginRow 
+				symbol = { symbol }
+				availableSymbols = { pre_origins }
+				origin 	= { obj.obj }
+				node 	= { node }
+				system 	= { system }
+				inCalc  = { calc.contains(symbol)  }
+			/>
+		{/each}
+		{#each pre_origins as symbol}
+			<div transition:slide|local class="derivedOriginRow" on:keydown={ ()=> fromPreOriginToOrigin(symbol) } on:click={ ()=> fromPreOriginToOrigin(symbol) } >
+				<div> {symbol} </div>
+				<div> Click To Add a Origin </div>
+			</div>
+		{/each}
 		
-		<OriginRow 
-			node ={ node }
-			system = { system }
-			deleteAllowed = {true}
-		/>
 	</div>
 </div>
--- src/Modules/ObsidianUI/UIInterfaces/Designer01/SystemDesigner/views/OriginRow.svelte
+++ src/Modules/ObsidianUI/UIInterfaces/Designer01/SystemDesigner/views/OriginRow.svelte
@@ -2,15 +2,37 @@
 
 	import CustomSelect from "../../BaseComponents/CustomSelect/CustomSelect.svelte";
 	import './OriginEditor.scss';
-	import { GrobDerivedNode, TTRPGSystem, GrobFixedNode} from "../../../../../../../src/Modules/Designer";
+	import { GrobDerivedNode, TTRPGSystem, GrobFixedNode, GrobDerivedOrigin} from "../../../../../../../src/Modules/Designer";
 	import Trash from '../../BaseComponents/buttons/Trash.svelte'
 	import Edit from '../../BaseComponents/buttons/Edit.svelte'
+    import { createEventDispatcher, onMount } from "svelte";
 	
 	export let node :GrobDerivedNode;
 	export let system:TTRPGSystem;
-	export let deleteAllowed : boolean = false;
+	export let inCalc : boolean = false;
+	export let symbol : string;
+	export let availableSymbols : string [] = [];
 
-	let targetNode : GrobDerivedNode | GrobFixedNode | null = null ;
+	let dispatch = createEventDispatcher();
+
+	export let origin : GrobDerivedOrigin | null = null ;
+	onMount(()=>{
+		if (origin){
+			let segments : string[] = [];
+			if (origin.origin){
+				segments = origin.origin.getLocationKeySegments();
+			}else{
+				segments = origin.originKey.split('.');
+			}
+ 
+			selectedLevel_0 = segments[0];
+			selectedLevel_1 = segments[1];
+			selectedLevel_2 = segments[2];
+
+			options_level1 = system.data[selectedLevel_0].getCollectionsNames();
+			options_level2 = system.data[selectedLevel_0].getCollection(selectedLevel_1).getNodeNames();
+		}
+	})
 
 	let selectedLevel_0 : string | null = null;
 	let selectedLevel_1 : string | null = null;
@@ -21,38 +43,44 @@
 	let options_level2 :string[] = [];
 
 	function onSelect( level : number , value : string ){
-		debugger
 		switch(level){
 			case 0:
-				if (value == 'fixed') {
-					options_level1 = system.fixed.getCollectionsNames()
-				} else {
-					options_level1 = system.derived.getCollectionsNames()
-				} 
+
+				options_level1 = system.data[value].getCollectionsNames();
 				selectedLevel_1 = null;
 				selectedLevel_2 = null;
 				options_level2 =[];
-				targetNode = null;
+				if (origin){
+					dispatch('deselectTargetNode');
+				}
+				origin = null;
 				break;
 			case 1:
 				//@ts-ignore
 				options_level2 = system.getCollection(selectedLevel_0,value)?.getNodeNames();
 				selectedLevel_2 = null;
-				targetNode = null;
+				if (origin){
+					dispatch('deselectTargetNode');
+				}
+				origin = null;
 				break;
 			case 2:
 				//@ts-ignore
-				targetNode = system.getNode(selectedLevel_0,selectedLevel_1,value);
+				let targetNode = system.getNode(selectedLevel_0,selectedLevel_1,value);
+				dispatch('foundTargetNode',targetNode);
 				break;
 		}
 	}
 
 	function onDeselect( level : number){
+		if (origin){
+			dispatch('deselectTargetNode');
+		}
 		switch(level){
 			case 0: 
 				options_level1 = [];
 				options_level2 =[];
-				targetNode = null;
+				origin = null;
 				selectedLevel_1 = null;
 				selectedLevel_2 = null;
 				break;
@@ -60,19 +88,28 @@
 				//@ts-ignore
 				options_level2 = [];
 				selectedLevel_2 = null;
-				targetNode = null;
+				origin = null;
 				break;
 			case 2:
 				//@ts-ignore
 				targetNode = null;
 				break;
 		}
+	}
 
+	function ondelete(){
+		dispatch('onDelete',symbol)
 	}
 
 </script>
 <div class="derivedOriginRow" >
-	<input type="text" maxlength=2 placeholder="@x" />
+	{#if availableSymbols.length == 0}
+		<div class="derivedOriginRowInteractionField" >{symbol}</div>
+	{:else}
+		<CustomSelect selected={symbol} options={ [...availableSymbols, symbol] }	on:onSelect={(e) => console.log(e.detail ?? 'hej')} />
+	{/if} 
+
+
 		<CustomSelect bind:selected={selectedLevel_0} options={ options_level0 }	on:onSelect={(e) => onSelect(0,e.detail)} on:onDeselect={()=>onDeselect(0)} />
 	{#key options_level1}
 		<CustomSelect bind:selected={selectedLevel_1} options={options_level1}		on:onSelect={(e) => onSelect(1,e.detail)} on:onDeselect={()=>onDeselect(0)} disabled={ options_level1.length == 0 }/>
@@ -80,11 +117,7 @@
 	{#key options_level2}
 		<CustomSelect bind:selected={selectedLevel_2} options={options_level2}		on:onSelect={(e) => onSelect(2,e.detail)} on:onDeselect={()=>onDeselect(0)} disabled={ options_level2.length == 0 }/>
 	{/key}
-	<div class="derivedOriginRowInteractionField" data-color={ deleteAllowed ? 'verbose' : 'good' }>
-		{#if deleteAllowed}
-			<Trash color={'white'}/>
-		{:else}
-			<Edit color={'white'}/>
-		{/if}
+	<div class="derivedOriginRowInteractionField" data-color={ inCalc ? 'verbose' : 'error' } on:click={ondelete} on:keydown={ondelete}>
+		<Trash color={'white'}/>
 	</div>
 </div>
