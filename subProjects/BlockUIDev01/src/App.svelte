cf5860d808697c059076b3d13e5ddbb0c4ebff6e
Grobax <43875102+GrobaxGrappleBlast@users.noreply.github.com>
2024-06-25 08:34:55 +0200

    progress with Exported App

---
+++ subProjects/BlockUIDev01/src/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let system:ITTPRGSystem;
+</script>
+<div>
+	HEJ HANS ;
+</div>
---
+++ subProjects/BlockUIDev01/src/mainApp.ts
@@ -0,0 +1,10 @@
+
+import App from './App.svelte'
+
+const app = new App(
+	//{
+	//		target: new TTRPGSystem(),
+	//}
+);
+
+export default app
