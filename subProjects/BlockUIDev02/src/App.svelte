5efe9e2f33bc1a1d9c44c9428ab7e73b16728554
Grobax <43875102+GrobaxGrappleBlast@users.noreply.github.com>
2024-08-15 16:00:23 +0200

    small dragging progress

--- subProjects/BlockUIDev02/src/App.svelte
+++ subProjects/BlockUIDev02/src/App.svelte
@@ -1,80 +1,145 @@
 <script  context="module"  lang="ts">
-class DragHandlerController {
-	
-	public data : Writable<SheetData>;
-	public constructor( data : Writable<SheetData>){
-		this.data = data;
-	}
-
-	public isDragging = false;
-	public pauseDragg = false;
-	public dragID : string;
-	public targetID: string;
-
-	public moveRow(){
+	class DragHandlerController {
 		
-		if (!this.isDragging){
-			return;
+		public data : Writable<SheetData>;
+		public constructor( data : Writable<SheetData>){
+			this.data = data;
 		}
 
-		if (!this.dragID || !this.targetID || this.targetID  == this.dragID ){
-			return;
-		}
+		public isDragging = false;
+		public pauseDragg = false;
+		public dragID : string;
+		public targetID: string;
 
-		this.data.update( list => {
+		public moveRow(){
+			
+			if (!this.isDragging){
+				return;
+			}
 
-			let a = this.findIndexOfID( this.dragID );
-			let b = this.findIndexOfID( this.targetID );
+			if (!this.dragID || !this.targetID || this.targetID  == this.dragID ){
+				return;
+			}
 
-			const temp = list.data[a];
-			this.pauseDragg=true;
-			setTimeout( () => { this.pauseDragg = false ; }, 100)
-			list.data[a] = list.data[b];
-			list.data[b] = temp;
- 
+			this.data.update( list => {
 
-			return list;
-		}) 
-		console.log(JSON.stringify(this.data))
-	}
+				let a = this.findIndexOfID( this.dragID );
+				let b = this.findIndexOfID( this.targetID );
 
-	onDragStart( e , id){
-		console.log('onDragStart')
-		this.isDragging = true;
-		this.dragID = id;
-
-		const target = e.target;
-		target.setAttribute('data-dragging','true')
-	}
-	onDragOver( e , id){
-		console.log('onDragOver')
-		if (!this.isDragging || this.pauseDragg ){
-			return;
-		}
-		this.targetID = id;
-		
-		
-		this.moveRow( );
-	}
-	onDragEnd( e , id){
-		console.log('onDragEnd')
-		this.isDragging = false;
-		this.dragID = null;
-		this.targetID = null;
-
-		const target = e.target;
-		target.setAttribute('data-dragging','false')
-		console.log(get(this.data));
-	}
-
-	findIndexOfID( id ){
-		return get(this.data).data.findIndex( p => p.id == id );
-	}
- 
-}
+				const temp = list.data[a];
+				this.pauseDragg=true;
+				setTimeout( () => { this.pauseDragg = false ; }, 100)
+				list.data[a] = list.data[b];
+				list.data[b] = temp;
 	
 
+				return list;
+			})  
+		}
 
+		onDragStart( e , id){  
+			this.isDragging = true;
+			this.dragID = id; 
+			const target = e.target;
+			target.setAttribute('data-dragging','true') 
+		}
+		onDragOver( e , id){  
+			if (!this.isDragging || this.pauseDragg ){
+				return;
+			}
+			this.targetID = id;
+			this.moveRow( ); 
+		}
+		onDragEnd( e , id){  
+			this.isDragging = false;
+			this.dragID = null;
+			this.targetID = null;
+			const target = e.target;
+			target.setAttribute('data-dragging','false') 
+		}
+
+		findIndexOfID( id ){
+			return get(this.data).data.findIndex( p => p.id == id );
+		} 
+	} 
+
+	class DragItemHandlerController {
+		
+		public data : Writable<SheetData>;
+		public constructor( data : Writable<SheetData>){
+			this.data = data;
+		}
+
+		public isDragging = false;
+		public pauseDragg = false;
+		public drag__ROW_ID : string;
+		public targe_ROW_ID: string;
+		public dragID : string;
+		public targetID: string;
+
+		public moveRowItem(){
+			
+			if (!this.isDragging){
+				return;
+			}
+			
+			if (!this.dragID || this.targetID  == this.dragID ){
+				return;
+			}
+			if (!this.drag__ROW_ID || !this.targe_ROW_ID  ){
+				return;
+			}
+
+			this.data.update( list => {
+
+				let row_a = this.findRowIndexOfID( this.drag__ROW_ID );
+				let row_b = this.findRowIndexOfID( this.targe_ROW_ID );
+				let a = this.findItemIndexOfID( this.drag__ROW_ID,this.dragID) ;
+				let b = this.targetID ? this.findItemIndexOfID( this.targe_ROW_ID,this.targetID) : 0;
+
+				const temp = list.data[row_a].data[a];
+				this.pauseDragg=true;
+				setTimeout( () => { this.pauseDragg = false ; }, 100)
+				list.data[row_a].data[a] = list.data[row_b].data[b];
+				list.data[row_b].data[b] = temp;
+	 
+				return list;
+			})  
+		}
+
+		onDragStart( e , row_id, id){  
+			this.isDragging = true; 
+			this.dragID = id; 
+			this.drag__ROW_ID = row_id;
+			const target = e.target;
+			target.setAttribute('data-dragging','true') 
+		}
+		onDragOver( e , row_id, id){  
+			if (!this.isDragging || this.pauseDragg ){
+				return;
+			}
+			this.targetID = id;
+			this.targe_ROW_ID = row_id; 
+			this.moveRowItem( ); 
+		}
+		onDragEnd( e, row_id, id){  
+			this.isDragging = false;
+			this.dragID			= null;
+			this.targetID		= null;
+			this.targe_ROW_ID	= null; 
+			this.drag__ROW_ID	= null;
+			const target = e.target;
+			target.setAttribute('data-dragging','false') 
+		}
+
+		findRowIndexOfID( id ){
+			return get(this.data).data.findIndex( p => p.id == id );
+		} 
+		findItemIndexOfID( rowId , id ){ 
+			const row = get(this.data).data.find( p => p.id == rowId ); 
+			return row.data.findIndex(p => p.id == id)
+		} 
+	} 
 </script> 
 <script lang="ts">  
 	import "./app.scss";
@@ -86,7 +151,7 @@ class DragHandlerController {
     import { get, type Writable, writable } from 'svelte/store';
     import { fly } from "svelte/transition";
 	import { flip } from 'svelte/animate';
-    
+	import { bounceInOut } from 'svelte/easing';
  
 	let editMode : boolean = true;
 	let _JSON = `{
@@ -145,7 +210,7 @@ class DragHandlerController {
 		ev.preventDefault();
 	}
 	let DragHandler = new DragHandlerController(OBJ);
-
+	let DragItemHandler = new DragItemHandlerController(OBJ);
 </script>
 <div class="Sheet">
 	<div>
@@ -162,7 +227,7 @@ class DragHandlerController {
 			on:dragend	={(e)=>DragHandler.onDragEnd	(e, row.id)}
 			on:dragover	={allowDrop}
 			
-			transition:fly={{duration:100}}
+			transition:fly={{duration:100, y:100}}
 			animate:flip={{duration:100}}
 			draggable="true"
 			> 
@@ -173,6 +238,14 @@ class DragHandlerController {
 				<div 
 					data-rowId={i}
 					data-itemId={j} 
+					transition:fly={{duration:100, y:100}}
+					animate:flip={{ duration:100 , easing: bounceInOut }} 
+					
+					on:dragstart={(e)=>DragItemHandler.onDragStart	(e, row.id, item.id ?? i )}
+					on:dragenter={(e)=>DragItemHandler.onDragOver	(e, row.id, item.id ?? i )}
+					on:dragend	={(e)=>DragItemHandler.onDragEnd	(e, row.id, item.id ?? i )}
+					on:dragover	={allowDrop}
+					draggable="true"
 				>
 					{#if item == null}
 						<EmptySpot 
